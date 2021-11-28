fa5b7f7bb8716e1dc8ba2f2c58e4a7695fc9a90a
yj1028 <jenie1589@naver.com>
2021-11-28 21:14:06 +0900

    first commit

---
+++ src/main/java/com/msix/admin/product/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.msix.admin.product.service;
+
+import java.util.List;
+
+import com.msix.admin.product.vo.ProductVO;
+
+public interface ProductService {
+	public List<ProductVO> productList(ProductVO pvo);
+}
